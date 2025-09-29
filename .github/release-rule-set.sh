4d163fe1df4ece06b0b1bfae3caf633a597c471a
77160860 <77160860@qq.com>
2025-09-29 20:11:15 +0800

    Update release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -21,7 +21,7 @@ cd sing-geosite/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/77160860/sing-box-geo.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/77160860/singbox-geo.git
 git branch -M rule-set-geosite
 git add .
 git commit -m "Update rule-set"
