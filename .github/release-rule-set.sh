cab09547cbb9cbc821fcf6153709329a15b52102
77160860 <77160860@qq.com>
2025-09-29 20:43:30 +0800

    Update release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
-
-set -e -o pipefail
-
 # geoip
-
 cd sing-geoip/rule-set
 git init
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
 git remote add origin https://github-action:$GITHUB_TOKEN@github.com/77160860/singbox-geo.git
-git branch -M geoip
+git checkout -B geoip
 git add .
 git commit -m "Update rule-set"
 git push -f origin geoip
 
-cd -
-# sing-geosite
-
+# geosite
 cd sing-geosite/rule-set
 git init
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
 git remote add origin https://github-action:$GITHUB_TOKEN@github.com/77160860/singbox-geo.git
-git branch -M geosite
+git checkout -B geosite
 git add .
 git commit -m "Update rule-set"
 git push -f origin geosite
+
